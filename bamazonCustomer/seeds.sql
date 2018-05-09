1fd0f448dea5e0523dee9143526e04662496f0bf
OpenPier <35052947+OpenPier@users.noreply.github.com>
2018-05-09 12:16:01 -0500

    2.0.1

---
+++ bamazonCustomer/seeds.sql
@@ -0,0 +1,13 @@
+
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sushi", "Food", 13.75, 53);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Protein Powder 5 lbs", "Health/Nutrition", 72.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Popcorn", "Food", 5, 107);
+
